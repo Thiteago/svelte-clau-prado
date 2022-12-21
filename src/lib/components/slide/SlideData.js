530a2c6fef1db24f836c705d4b9fef9299233983
Thiago <thiagodavidpd@gmail.com>
2022-12-21 17:15:16 -0300

    removido slide

--- src/lib/components/slide/SlideData.js
+++ src/lib/components/slide/SlideData.js
@@ -1,13 +1,8 @@
-import imagem from './../../assets/img/produto-1.jpeg'
 import imagem2 from './../../assets/img/produto-3.jpeg'
 import imagem3 from './../../assets/img/produto-5.jpg'
 
 export const SlideData = [
-    {
-        image: imagem,
-        title: 'El Bruxo',
-        describe: 'Rasta a tabaca na vara',
-    },
+
     {
         image: imagem2,
         title: 'El Unicórnio',
