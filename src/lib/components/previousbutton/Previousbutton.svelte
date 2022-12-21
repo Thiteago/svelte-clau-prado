6a221b4173aaf24c706c751c51511c9d98d96c56
Thiago <thiagodavidpd@gmail.com>
2022-12-21 17:16:06 -0300

    adicionado e criado componente previousbutton

---
+++ src/lib/components/previousbutton/Previousbutton.svelte
@@ -0,0 +1,11 @@
+<script>
+  import './style.scss'
+  export let endereco = ''
+</script>
+
+<a href={endereco}>
+  <div class="container-botao">
+      <a href="/#" class="back botao">&#8249;</a>
+      <p>Voltar</p>
+  </div>
+</a>
