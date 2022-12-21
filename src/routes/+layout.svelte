2ead02da48c13d33b279f856b11b867f10bf7c8e
Thiago <thiagodavidpd@gmail.com>
2022-12-21 17:14:38 -0300

    adicionado import de fontes do google no layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+  import "../app.css";
+</script>
+
+<svelte:head>
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Viga&display=swap" rel="stylesheet">
+</svelte:head>
+
+<slot />
