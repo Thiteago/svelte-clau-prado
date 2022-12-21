db6be94ad1ac7025e72bb4aa0b7470a165467ffc
Thiago <thiagodavidpd@gmail.com>
2022-12-21 17:17:08 -0300

    criado e estilizado componente de busca

---
+++ src/lib/components/busca/Busca.svelte
@@ -0,0 +1,12 @@
+<script>
+  import './style.scss'
+
+</script>
+
+
+<section class="main-busca">
+  <div class="wrapper-busca">
+      <input type="text" />
+      <button>Buscar</button>
+  </div>
+</section>
