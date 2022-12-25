304799bd168d94fcfe81116f8bbb7b6972d93964
Thiago <thiagodavidpd@gmail.com>
2022-12-25 18:51:38 -0300

    criação da storage de autenticação

--- src/lib/stores/login.js
+++ src/lib/stores/login.js
@@ -1,3 +1,34 @@
 import { writable } from "svelte/store";
+import { browser } from '$app/environment';
 
-export const user = writable({})
+
+export const user = writable({
+  cargo: '',
+  email: '',
+  id: 0,
+  nome: ''
+})
+
+export const signed = writable(false)
+
+export function logout(){
+  localStorage.clear()
+  user.set({
+    cargo: '',
+    email: '',
+    id: 0,
+    nome: ''
+  })
+  signed.set(false)
+}
+
+export function loadStorageData(){
+  if(browser){
+    user.set(JSON.parse(localStorage.getItem("@Auth:user"))) 
+    if(localStorage.getItem("@Auth:user")!= null){
+     signed.set(true)
+    }else{
+     signed.set(false)
+    }
+  }
+}
