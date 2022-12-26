f589d2d8f3d7577f23e02ae17c576ad235a892e7
Thiago <thiagodavidpd@gmail.com>
2022-12-25 21:59:15 -0300

    criado componente com conteudo inicial da pagina perfil

---
+++ src/lib/components/perfil/Perfil.svelte
@@ -0,0 +1,44 @@
+<script>
+  import foto from '$lib/assets/img/foto-clau.png'
+  
+  export let user
+</script>
+
+
+<div class="profileContainer">
+  <div class="userWrapper">
+      <div>
+          <h2>Bem-vindo, {user.nome}!</h2>
+          <span>{user.email}</span>
+      </div>
+      <div class="containerImageProfile">
+          <img class="imageProfile" src={foto} alt="" />
+      </div>
+  </div>
+  
+  <div class="containerUltimoPedido">
+      <h3 class="sectionTitle">Ultimo Pedido</h3>
+      <div class="containerPedido">
+          <div class="wrapperPedido">
+              <div>
+                  <h4 class="title">NÚMERO DO PEDIDO</h4>
+                  <span>#1</span>
+              </div>
+              <div>
+                  <h4 class="title">STATUS</h4>
+                  <span>Concluído</span>
+              </div>
+              <div>
+                  <h4 class="title">DATA</h4>
+                  <span>01/01/2022</span>
+              </div>
+              <div>
+                  <h4 class="title">PAGAMENTO</h4>
+                  <span>Cartão de crédito</span>
+              </div>
+          </div>
+          <a href="/#">Histórico de Pedidos</a>
+      </div>
+      
+  </div>
+</div>
