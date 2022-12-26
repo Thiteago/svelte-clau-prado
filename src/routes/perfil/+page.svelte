8ffd01f7a9d3e3eb14d9e4443d7b4471f332f40f
Thiago <thiagodavidpd@gmail.com>
2022-12-25 21:58:59 -0300

    criado perfil e estilizado

---
+++ src/routes/perfil/+page.svelte
@@ -0,0 +1,44 @@
+<script>
+  import { goto } from "$app/navigation";
+  import Perfil from "$lib/components/perfil/Perfil.svelte";
+  import {user, loadStorageData, signed} from '$lib/stores/login'
+	import { redirect } from "@sveltejs/kit";
+  import "./style.scss"
+
+  $: selected = 'Perfil'
+
+  export function load(){
+    loadStorageData()
+    if($signed == false){
+      console.log('oi')
+      redirect(300,"/")
+    }
+  }
+
+  load()
+
+</script>
+
+
+<div class="containerContent">
+  <aside class="menuBar">
+    <span class="returnButton" on:keyup={() => {goto("/")}} on:click={() => {goto("/")}}>Voltar</span>
+    <ul class="menu">
+        <li class="item" on:keyup={() => {selected = 'Perfil'}} on:click={() => {selected = 'Perfil'}}>Perfil</li>
+        <li class="item" on:keyup={() => {selected = 'Dados Cadastrais'}} on:click={() => {selected = 'Dados Cadastrais'}}>Dados Cadastrais</li>
+        <li class="item" on:keyup={() => {selected = 'Pagamento'}} on:click={() => {selected = 'Pagamento'}}>Formas de Pagamento</li>
+        <li class="item" on:keyup={() => {selected = 'Historico de Pedidos'}} on:click={() => {selected = 'Historico de Pedidos'}}>Historico de Pedidos</li>
+        <li class="item" on:keyup={() => {selected = 'Promoções'}} on:click={() => {selected = 'Promoções'}}>Promoções</li>
+        <li class="item" on:keyup={() => {selected = 'Produtos'}} on:click={() => {selected = 'Produtos'}}>Produtos</li>
+        <li class="item" on:keyup={() => {selected = 'Relatórios'}} on:click={() => {selected = 'Relatórios'}}>Relatórios</li>
+        <li class="item" on:keyup={() => {selected = 'Estoque'}} on:click={() => {selected = 'Estoque'}}>Estoque</li>
+        <li class="item" on:keyup={() => {selected = 'Permissoes'}} on:click={() => {selected = 'Permissoes'}}>Permissões</li>
+        <li class="item" on:keyup={() => {selected = 'Usuarios do Sistema'}} on:click={() => {selected = 'Usuarios do Sistema'}}>Usuarios Administrativos</li>
+    </ul>
+  </aside>
+  <div class="contentWrapper">
+    {#if selected == 'Perfil'}
+      <Perfil user={$user}/>
+    {/if}
+  </div>
+</div>
