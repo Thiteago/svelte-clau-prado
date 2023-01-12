ea147e9e006492a66c5beb43b38b3804d1b2cccc
Thiago <thiagodavidpd@gmail.com>
2023-01-12 20:29:44 -0300

    adicionado e estilizado carrinho no header

--- src/lib/components/header/Header.svelte
+++ src/lib/components/header/Header.svelte
@@ -2,12 +2,13 @@
 import {user, signed, logout} from '$lib/stores/login'
 import './style.scss'
 import avatar from '$lib/assets/img/avatar-login.png'
-import logomin from '$lib/assets/img/logo-clau.png'
-import logobig from '$lib/assets/img/logomin.jpg'
+import logobig from '$lib/assets/img/logo-clau.png'
+import logomin from '$lib/assets/img/logomin.jpg'
 import cart from  '$lib/assets/icons/cart2.svg'
 
 export let tamanho = "grande"
 let logo
+let carrinhoAtivo = false
 
 if(tamanho == "grande"){
     logo = logobig
@@ -18,24 +19,67 @@ if(tamanho == "grande"){
 </script>
 <header class="main-header">
 {#if !$signed}
-    <div class="wrapper-logo">
-      <img src={logo} alt="logo clau prado"/>
+    <div class="container-left-content">
+        <div class="wrapper-logo {tamanho == "grande" ? "" : "-pequeno"}">
+        <img src={logo} alt="logo clau prado"/>
+        </div>
+        <nav class="nav">
+            <a class="main-action" href={"/#"}>Inicio</a>
+            <a class="main-action" href={"/produtos"}>Produtos</a>
+            <a class="main-action" href={"/mandesuaideia"}>Mande sua Ideia</a>
+            <a class="main-action" href={"/sobre"}>Sobre</a>
+        </nav>
     </div>
-    <nav class="nav">
-        <a class="main-action" href={"/#"}>Inicio</a>
-        <a class="main-action" href={"/produtos"}>Produtos</a>
-        <a class="main-action" href={"/mandesuaideia"}>Mande sua Ideia</a>
-        <a class="main-action" href={"/sobre"}>Sobre</a>
-    </nav>
     
-    <div class="cart-container">
-      <img src={cart} alt="">
-    </div>
 
-    <div class="wrapper-login">
-        <a class="main-action -second" href={"/login"}>Login</a>
-        <a class="main-action -third" href={"/login"}>ou Cadastre-se</a>
-        <img src={avatar} alt="imagem para cadastro" height="105" class="img_avatar"/>
+    <div class="container-right-content">
+        <div class="cart-container">
+            <img on:click={() => carrinhoAtivo == true ? carrinhoAtivo = false : carrinhoAtivo = true} src={cart} alt="">
+            <span>0 itens no carrinho</span>
+        </div>
+        {#if carrinhoAtivo == true}
+            <div class="cart-preview-container">
+                <h2 class="title">Alguns produtos no seu carrinho</h2>
+                <div class="wrapper-produtos">
+                    <div class="produto">
+                        <div class="produto-info">
+                            <h3>Produto 1</h3>
+                            <div class="adicional-info-wrapper">
+                                <span>Quantidade: 1</span>
+                                <span>R$ 10,00</span>
+                            </div>
+                            
+                        </div>
+                    </div>
+                    <div class="produto">
+                        <div class="produto-info">
+                            <h3>Produto 1</h3>
+                            <div class="adicional-info-wrapper">
+                                <span>Quantidade: 1</span>
+                                <span>R$ 10,00</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="produto">
+                        <div class="produto-info">
+                            <h3>Produto 1</h3>
+                            <div class="adicional-info-wrapper">
+                                <span>Quantidade: 1</span>
+                                <span>R$ 10,00</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="wrapper-button-cart">
+                        <button>Ir para carrinho</button>
+                    </div>
+                </div>
+            </div>
+        {/if}
+        <div class="wrapper-login">
+            <a class="main-action -second" href={"/login"}>Login</a>
+            <a class="main-action -third" href={"/login"}>ou Cadastre-se</a>
+            <img src={avatar} alt="imagem para cadastro" height="105" class="img_avatar"/>
+        </div>
     </div>
 {:else}
     <img src={logo} alt="logo clau prado"/>
