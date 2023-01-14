0fdd62243b0db559d17a7ad428f55047058be3e7
Thiago <thiagodavidpd@gmail.com>
2023-01-14 17:10:56 -0300

    added node-correios package to freight calculate

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,9 @@
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
+    fontFamily: {
+      sans: ['poppins', 'sans-serif'],
+    },
     extend: {},
   },
   plugins: [require("daisyui")],
