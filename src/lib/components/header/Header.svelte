9cbacabab1177225ab907b4e4ee10e061c295083
Thiago <thiagodavidpd@gmail.com>
2023-01-14 18:29:15 -0300

    added access to the cart on signed user context

--- src/lib/components/header/Header.svelte
+++ src/lib/components/header/Header.svelte
@@ -37,8 +37,8 @@ if(tamanho == "grande"){
     <div class="container-right-content">
         <div class="cart-container">
             {#if $page.url.pathname != "/carrinho"}
-            <img on:click={() => carrinhoAtivo == true ? carrinhoAtivo = false : carrinhoAtivo = true} src={carts} alt="">
-            <span>{$cart.length} itens no carrinho</span>
+                <img on:click={() => carrinhoAtivo == true ? carrinhoAtivo = false : carrinhoAtivo = true} src={carts} alt="">
+                <span>{$cart.length} itens no carrinho</span>
             {/if}
         </div>
         {#if carrinhoAtivo == true}
@@ -75,19 +75,58 @@ if(tamanho == "grande"){
         </div>
     </div>
 {:else}
-    <img src={logo} alt="logo clau prado"/>
-    <nav class="nav">
-        <a class="main-action" href={"/#"}>Inicio</a>
-        <a class="main-action" href={"/produtos"}>Produtos</a>
-        <a class="main-action" href={"/mandesuaideia"}>Mande sua Ideia</a>
-        <a class="main-action" href={"/sobre"}>Sobre</a>
-    </nav>
-    
-    <div class="wrapper-login -logado">
-        <a href={"/login"} class="main-action -secondlogado">Ola! <br /> {$user.nome.split(' ')[0]}</a>
-        <div class="container-options">
-            <a class="sair-btn" href={"/perfil"}>Minha conta</a>
-            <a class="sair-btn" href={"/"} on:click={logout}>Sair</a>
+    <div class="container-left-content">
+        <div class="wrapper-logo {tamanho == "grande" ? "" : "-pequeno"}">
+        <img src={logo} alt="logo clau prado"/>
+        </div>
+        <nav class="nav">
+            <a class="main-action" href={"/#"}>Inicio</a>
+            <a class="main-action" href={"/produtos"}>Produtos</a>
+            <a class="main-action" href={"/mandesuaideia"}>Mande sua Ideia</a>
+            <a class="main-action" href={"/sobre"}>Sobre</a>
+        </nav>
+    </div>
+
+    <div class="container-right-content">
+        <div class="cart-container">
+            {#if $page.url.pathname != "/carrinho"}
+                <img on:click={() => carrinhoAtivo == true ? carrinhoAtivo = false : carrinhoAtivo = true} src={carts} alt="">
+                <span>{$cart.length} itens no carrinho</span>
+            {/if}
+        </div>
+        {#if carrinhoAtivo == true}
+            <div class="cart-preview-container">
+                <h2 class="title">Alguns produtos no seu carrinho</h2>
+                <div class="wrapper-products">
+                    {#if $cart.length > 0}
+                        {#each $cart as produto}
+                            <div class="produto">
+                                <div class="produto-info">
+                                    <h3>{produto.nome}</h3>
+                                    <div class="adicional-info-wrapper">
+                                        <span>Quantidade: 1</span>
+                                        <span>R$ {produto.valor}</span>
+                                    </div>
+                                </div>
+                            </div>
+                        {/each}
+                    {:else}
+                        <div class="text-center mt-2">
+                            <h3>Nenhum produto no carrinho</h3>
+                        </div>
+                    {/if}
+                    <div class="wrapper-button-cart">
+                        <a href="/carrinho">Ir para carrinho</a>
+                    </div>
+                </div>
+            </div>
+        {/if}
+        <div class="wrapper-login -logado">
+            <a href={"/login"} class="main-action -secondlogado">Ola! <br /> {$user.nome.split(' ')[0]}</a>
+            <div class="container-options">
+                <a class="sair-btn" href={"/perfil"}>Minha conta</a>
+                <a class="sair-btn" href={"/"} on:click={logout}>Sair</a>
+            </div>
         </div>
     </div>
 {/if}
