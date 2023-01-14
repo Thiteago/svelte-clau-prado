ad37a4ed3e3f7363d04e854ab551b833e227abd4
Thiago <thiagodavidpd@gmail.com>
2023-01-14 18:27:58 -0300

    create freightCalculate input

--- src/routes/carrinho/+page.svelte
+++ src/routes/carrinho/+page.svelte
@@ -5,11 +5,28 @@
   import location from '$lib/assets/icons/location-pin.svg'
   import './carrinho.scss'
 
-
   const optionsCEP = {
     mask: '00000-000',
     lazy: true
   };
+  $: cepValidates = true
+  let cep = '';
+
+  function freightCalculate(cep){
+    console.log(cep.length)
+    if(cep?.length < 9 || cep == ''){
+      console.log(cep + 'oiii')
+      cepValidates = false
+      return
+    }else{
+      cepValidates = true
+      cep = cep.replace('-', '')
+      console.log(cep)
+    }
+    // let response = fetch(`http://localhost:3333/carrinho/frete/${cep}`)
+  }
+
+
 </script>
 
 <Header />
@@ -25,9 +42,14 @@
           <img class="w-6 h-6" src={location} alt="">
           <h1 class="text-xl font-poppins font-bold">Calcular prazo e frete</h1>
         </div>
-        <div class="mt-3 flex gap-2 items-center">
-          <input type="text" placeholder="CEP" use:imask={optionsCEP} class="input input-bordered w-full max-w-xs" />
-          <button class="btn btn-secondary">Calcular</button>
+        <div class="mt-3">
+          <form  class="flex gap-2 items-center" action="#">
+            <input required bind:value={cep} type="text" placeholder="CEP" use:imask={optionsCEP} class="input input-bordered w-full max-w-xs" />
+            <button type="submit" class="btn btn-secondary" on:click={() => freightCalculate(cep)}>Calcular</button>
+          </form>
+          {#if cepValidates != true}
+            <span class="text-red-500 font-bold block mt-2">CEP Inválido!</span>
+          {/if}
         </div>
       </div>
     </div>
