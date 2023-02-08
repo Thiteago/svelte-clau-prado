4ccd7f2af5cab62da5885ca3565c966d221ba18a
Thiago <thiagodavidpd@gmail.com>
2023-02-08 16:14:39 -0300

    make freight calculation and layout of product list on cart

--- src/routes/carrinho/+page.svelte
+++ src/routes/carrinho/+page.svelte
@@ -2,7 +2,11 @@
   import Header from '$lib/components/header/Header.svelte';
 	import Steps from '$lib/components/steps/Steps.svelte';
   import { imask } from '@imask/svelte'
+  import { cart } from '$lib/stores/cart'
+  import leftArrow from '$lib/assets/icons/left-arrow.svg'
+  import rightArrow from '$lib/assets/icons/right-arrow-qtde.svg'
   import location from '$lib/assets/icons/location-pin.svg'
+  import resume from '$lib/assets/icons/resume-cart.svg'
   import './carrinho.scss'
 
   const optionsCEP = {
@@ -11,19 +15,26 @@
   };
   $: cepValidates = true
   let cep = '';
+  let freightInfo = {}
 
-  function freightCalculate(cep){
-    console.log(cep.length)
+  async function freightCalculate(){
     if(cep?.length < 9 || cep == ''){
-      console.log(cep + 'oiii')
       cepValidates = false
       return
     }else{
       cepValidates = true
       cep = cep.replace('-', '')
-      console.log(cep)
     }
-    // let response = fetch(`http://localhost:3333/carrinho/frete/${cep}`)
+    if(cepValidates){
+      await fetch(`http://localhost:3333/carrinho/frete/${cep}`)
+      .then(res => res.json())
+      .then(data => {
+        freightInfo = data
+        if(freightInfo?.valorpac == '0,00' && freightInfo?.valorsedex == '0,00'){
+          cepValidates = false
+        }
+      })
+    }
   }
 
 
@@ -36,24 +47,88 @@
   </div>
 
   <div class="flex w-9/12 mt-8">
-    <div>
+    <div class="flex w-full gap-10">
       <div>
         <div class="flex items-center gap-2 ">
           <img class="w-6 h-6" src={location} alt="">
           <h1 class="text-xl font-poppins font-bold">Calcular prazo e frete</h1>
         </div>
         <div class="mt-3">
-          <form  class="flex gap-2 items-center" action="#">
+          <form class="flex gap-2 items-center" on:submit={freightCalculate}>
             <input required bind:value={cep} type="text" placeholder="CEP" use:imask={optionsCEP} class="input input-bordered w-full max-w-xs" />
-            <button type="submit" class="btn btn-secondary" on:click={() => freightCalculate(cep)}>Calcular</button>
+            <button type="submit" class="btn btn-secondary">Calcular</button>
           </form>
           {#if cepValidates != true}
             <span class="text-red-500 font-bold block mt-2">CEP Inválido!</span>
           {/if}
         </div>
+        {#if Object.keys(freightInfo).length > 0 && freightInfo?.valorpac != '0,00' && freightInfo?.valorsedex != '0,00'}
+        <div>
+          <div class="mt-4">
+            <h1 class="text-xl font-poppins font-bold">Frete</h1>
+            <div class="mt-2">
+              <div class="flex justify-between">
+                <span class="text-gray-500">PAC - Até {freightInfo?.prazopac} dias úteis</span>
+                <span class="text-gray-500">R$ {freightInfo?.valorpac}</span>
+              </div>
+              
+              <div class="flex justify-between">
+                <span class="text-gray-500">SEDEX - Até {freightInfo?.prazosedex} dias úteis</span>
+                <span class="text-gray-500">R$ {freightInfo?.valorsedex}</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/if}
+      </div>
+      <div class="flex flex-col w-3/5">
+        {#if $cart.length > 0}
+          {#each $cart as product}
+          <div class="flex items-center justify-evenly w-full">
+            <div class="w-1/12">
+              <div class="w-20 h-20">
+                <img class="max-w-full" src='http://localhost:3333/static/{product.imagens}' alt="">
+              </div>
+            </div>
+            <div class="flex flex-col justify-center w-2/5">
+              <span class="text-black font-bold">{product.nome}</span>
+              <span class="text-gray-500 block">Voce esta <span class="font-bold text-black">{product.tipo == 'Aluguel' ? 'ALUGANDO' : 'COMPRANDO'}</span> esse produto</span>
+            </div>
+            <div class="flex flex-col items-center justify-center w-1/12">
+              <div class="flex flex-col items-center">
+                <label for="quantidade">Quant.</label>
+                <div class="flex items-center justify-center">
+                  <button on:click={() => {product.quantidade > 1 ? product.quantidade-- : ''}} class="w-4">
+                    <img src={leftArrow} alt="">
+                  </button>
+                  <input name="quantidade" class="input-number w-11 text-center" min="1" type="number" value={product.quantidade} readonly>
+                  <button on:click={() => product.quantidade++} class="w-4">
+                    <img src={rightArrow} alt="">
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div class="text-right w-1/5">
+              <p>Preço à vista</p>
+              <p class="font-bold text-[#F000B8]">R$ {product.valor},00</p>
+            </div>
+          </div>
+          {/each}
+        {/if}
+        {#if $cart.length == 0}
+          <div class="flex flex-col items-center absolute top-1/2 right-1/2">
+            <div class="w-20 h-20">
+              <img class="max-w-full" src="" alt="">
+            </div>
+            <span class="text-gray-500 mt-2">Seu carrinho está vazio :&#40 </span>
+          </div>
+        {/if}
+      </div>
+      <div>
+        <div>
+          <h1 class="flex gap-1 items-center font-bold text-2xl"><span><img class="w-5" src={resume} alt="resume logo"></span>Resumo</h1>
+        </div>
       </div>
     </div>
-    <aside></aside>
   </div>
-
 </div>
