f277f11cd77aac4366dce0c83fe72242784b63a2
thiteago <ti+thiago@seguroviagem.srv.br>
2023-02-28 22:26:46 -0300

    create modal to show order details

---
+++ src/lib/components/historicopedidos/Historico.svelte
@@ -0,0 +1,61 @@
+<script>
+  import {fetchSales} from '$lib/js/helpers.js'
+	import { onMount } from 'svelte';
+	import Modal from '../modal/Modal.svelte';
+
+  export let user
+  let isOpen = false
+  $: selectedPedido = {}
+  let pedidos = []
+
+  async function loadSales(){
+    pedidos = await fetchSales(user.id)
+  }
+
+  onMount(async () => {
+    await loadSales()
+  })
+
+  function selectPedido(id){
+    selectedPedido = pedidos.find(pedido => pedido.id === id)
+    isOpen = true
+  }
+
+</script>
+
+<section class="p-6">
+  <h1 class="text-2xl">Histórico de Pedidos</h1>
+  <div class="mt-10">
+    <table class="table-auto">
+      <thead>
+        <tr>
+          <th class="px-4 py-2">Número do Pedido</th>
+          <th class="px-4 py-2">Status</th>
+          <th class="px-4 py-2">Data</th>
+          <th class="px-4 py-2">Pagamento</th>
+        </tr>
+      </thead>
+      <tbody>
+        {#if pedidos.length === 0}
+          <tr>
+            <td class="border px-4 py-2" colspan="4">Nenhum pedido encontrado</td>
+          </tr>
+        {:else}
+          {#each pedidos as pedido}
+            <tr class="text-center">
+              <td class="border px-4 py-2">{pedido.id}</td>
+              <td class="border px-4 py-2">{pedido.status}</td>
+              <td class="border px-4 py-2">{pedido.data_pedido}</td>
+              <td class="border px-4 py-2">{pedido.Pagamento.forma_pagamento}</td>
+              <!-- svelte-ignore a11y-click-events-have-key-events -->
+              <td><label for="my-modal-{pedido.id}" on:click={() => selectPedido(pedido.id)} class="btn btn-primary rounded">Detalhes</label></td>
+            </tr>
+            
+          {/each}
+        {/if}
+      </tbody>
+    </table>
+  </div>
+</section>
+
+<Modal pedido={selectedPedido} {isOpen}/>
---
+++ src/lib/components/modal/Modal.svelte
@@ -0,0 +1,19 @@
+<script>
+  export let pedido
+  export let isOpen
+
+  $: if(pedido){
+    console.log(pedido)
+  }
+</script>
+
+{#if pedido != null}
+  <input type="checkbox" checked={isOpen} id="my-modal-{pedido.id}" class="modal-toggle" />
+  <!-- svelte-ignore a11y-click-events-have-key-events -->
+  <label on:click={() => {isOpen = false}} for="my-modal-{pedido.id}" class="modal">
+    <label class="modal-box relative" for="">
+      <h3 class="text-lg font-bold">{pedido.id} random Internet user!</h3>
+      <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+    </label>
+  </label>
+{/if}
