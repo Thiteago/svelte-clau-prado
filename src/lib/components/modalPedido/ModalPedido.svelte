3a0b8d27d752d8231c156e852e81bb074af8dfeb
Thiago <thiagodavidpd@gmail.com>
2023-03-04 02:01:42 -0300

    change modalPedido to show more data about the order

--- src/lib/components/modalPedido/ModalPedido.svelte
+++ src/lib/components/modalPedido/ModalPedido.svelte
@@ -1,4 +1,5 @@
 <script>
+  import order from '$lib/assets/icons/order-icon.svg'
   export let pedido
   export let isOpen
 
@@ -12,8 +13,37 @@
   <!-- svelte-ignore a11y-click-events-have-key-events -->
   <label on:click={() => {isOpen = false}} for="my-modal-{pedido.id}" class="modal">
     <label class="modal-box relative" for="">
-      <h3 class="text-lg font-bold">{pedido.id} random Internet user!</h3>
-      <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+      <h3 class="text-lg font-bold flex items-center"><img class="h-8 w-8" src={order} alt="order icon"/>Pedido #{pedido.id}</h3>
+      <div class="mt-8 flex flex-col gap-5">
+        <div>
+          <h2 class="font-bold">Data do Pedido:</h2>
+          <p>{pedido.data_pedido}</p>
+        </div>
+        
+        <div>
+          <h2 class="font-bold">Endereço:</h2>
+          <p>{pedido.endereco.rua}, {pedido.endereco.numeroRua} - {pedido.endereco.bairro}</p>
+          <p>{pedido.endereco.cidade}, {pedido.endereco.estado} <br/> {pedido.endereco.cep}</p>
+        </div>
+
+        <div>
+          {#if pedido.Pagamento.forma_pagamento == 'boleto'}
+          <h2 class="font-bold">Forma de pagamento: <i>Boleto</i> </h2>
+            <div class="flex items-center mt-2 gap-5">
+              <button class="btn btn-success">Baixar Boleto</button>
+              <p>Clique no Botao para baixar seu boleto</p>
+            </div>
+            
+          {:else if pedido.forma_pagamento == 'cartao'}
+            <p>Cartão</p>
+          {/if}
+        </div>
+
+        <div>
+          <h2 class="font-bold">Método de Entrega:</h2>
+          
+        </div>
+      </div>
     </label>
   </label>
 {/if}
