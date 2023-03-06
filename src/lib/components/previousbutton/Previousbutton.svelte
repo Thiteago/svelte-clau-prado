c687288b8c8ecef04667da1c2cc9e467881995fe
Thiago <thiagodavidpd@gmail.com>
2023-03-06 15:25:01 -0300

    changed previous button

--- src/lib/components/previousbutton/Previousbutton.svelte
+++ src/lib/components/previousbutton/Previousbutton.svelte
@@ -5,7 +5,7 @@
 
 <a href={endereco}>
   <div class="container-botao">
-      <a href="/#" class="back botao">&#8249;</a>
-      <p>Voltar</p>
+    <a href="/#" class="back botao">&#8249;</a>
+    <p>Voltar</p>
   </div>
 </a>
