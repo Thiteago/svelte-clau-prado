baafbb13db4ce61fc37a8fdab618b00aeaf34d47
Thiago <thiagodavidpd@gmail.com>
2023-03-07 12:52:50 -0300

    renamed style file from previousbutton

--- src/lib/components/previousbutton/Previousbutton.svelte
+++ src/lib/components/previousbutton/Previousbutton.svelte
@@ -1,11 +1,11 @@
 <script>
-  import './style.scss'
+  import './previousButton.scss'
   export let endereco = ''
 </script>
 
 <a href={endereco}>
   <div class="container-botao">
-    <a href="/#" class="back botao">&#8249;</a>
+    <a href="/#" class="voltar botao">&#8249;</a>
     <p>Voltar</p>
   </div>
 </a>
