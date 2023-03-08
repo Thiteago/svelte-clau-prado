833ccc7fc26f7e4191b00b95e7425fc1ec297191
Thiago <thiagodavidpd@gmail.com>
2023-03-08 15:38:58 -0300

    create modal to show promotion details

---
+++ src/lib/components/modalPromocao/modalPromocao.svelte
@@ -0,0 +1,14 @@
+<script>
+  export let promocao
+
+</script>
+  <input type="checkbox" id="my-modal-{promocao.id}" class="modal-toggle" />
+  <!-- svelte-ignore a11y-click-events-have-key-events -->
+  <label on:click={() => {isOpen = false}} for="my-modal-{promocao.id}" class="modal">
+    <label class="modal-box relative" for="">
+      <h3 class="text-lg font-bold flex items-center">promocao #{promocao.id}</h3>
+      <div class="mt-8 flex flex-col gap-5">
+        
+      </div>
+    </label>
+  </label>
