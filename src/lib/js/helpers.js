9b517cfd9d0bc5982ac71e04f4752d02b261de88
Thiago <thiagodavidpd@gmail.com>
2023-03-10 14:45:29 -0300

    added new functions to helper.js

--- src/lib/js/helpers.js
+++ src/lib/js/helpers.js
@@ -16,15 +16,38 @@ export async function fetchSales(userId){
   return []
 }
 
+export async function calculateDiscount(products){
+  products.forEach((item) => {
+    if(item.promocao != null && item.promocao.status == 'Ativo'){
+      if(item.promocao.tipo == 'valor_fixo'){
+        item.valor = item.valor - item.promocao.valor_desconto
+      } else {
+        item.valor = item.valor - (item.valor * (item.promocao.valor_desconto / 100))
+      }
+    }
+  })
+  return products
+}
+
 export async function fetchProducts(){
   const response = await fetch('http://localhost:3333/Produto')
   if(response.status === 200){
-    const data = await response.json()
+    let data = await response.json()
     if(data.length > 0){
+      data = await calculateDiscount(data)
       return data
     }
   }
   return []
+}
+
+export async function fetchProductsById(id){
+  const response = await fetch(`http://localhost:3333/Produto/${id}`)
+  let data = await response.json()
+  data.dataFabricacao = formatDate(data.dataFabricacao)
+  await calculateDiscount([data])
+
+  return data
 }
 
 export async function fetchPromotions(){
@@ -36,6 +59,19 @@ export async function fetchPromotions(){
     }
   }
   return []
+}
+
+export async function fetchDisable(id){
+  const response = await fetch(`http://localhost:3333/promocao/desabilitar/${id}`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+  if(response.status === 200){
+    return true
+  }
+  return false
 }
 
 export function formatDate(date){
