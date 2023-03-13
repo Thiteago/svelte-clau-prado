b845b772a25231a1a736d9146ff8ef6bdfdd6f0e
Thiago <thiagodavidpd@gmail.com>
2023-03-13 16:15:08 -0300

    change  structure and style  of  produto component of produtos page

--- src/lib/components/produto/Produto.svelte
+++ src/lib/components/produto/Produto.svelte
@@ -1,33 +1,34 @@
 <script>
 	import { onMount } from 'svelte';
   import cifrao from '$lib/assets/icons/cifrao.svg'
-  import './style.scss'
-  export let id, title, description, type, buttonType, valor
+  import { calculateDiscount } from '$lib/js/helpers.js'
+  import './produto.scss'
+  export let data
 
-  $: images = ''
-  valor = valor.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})
+  let promotionalValue
+  let formatedValue = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(data.valor)
 
-  async function getImages(){
-    const response = await fetch(`http://localhost:3333/Produto/ImagePath/${id}`)
-    const data = await response.json()
-    images = data.caminhos[0]
-  }
 
-  onMount(() => {
-    getImages()
+  onMount(async () => {
+    promotionalValue = await calculateDiscount(data)
   })
 </script>
 
 <div class='wrapper-produtos'>
   <div class='icon-produto'>
-      {#if images != undefined && images != ''}
-        <img class="object-cover w-60 h-60" src={`http://localhost:3333/static/${images}`} alt="" />
-      {/if}
+    <img class="object-cover w-60 h-60" src={`http://localhost:3333/static/${data.caminhos[0]}`} alt="" />
   </div>
   <div class='info-produto'>
-      <div class='descricao-produto'>
-          <h1 class="text-3xl font-bold">{title}</h1>
-          <p>{description}</p>
+      <div class="flex justify-between pr-5">
+        <div class='descricao-produto'>
+          <h1 class="text-3xl font-bold">{data.nome}</h1>
+          <p>{data.descricao}</p>
+        </div>
+        {#if data.promocao != null}
+          <div >
+            <span class="bg-[#7C3267] rounded p-2 text-white">Item em Promoçao</span>
+          </div>
+        {/if}
       </div>
       
       <div class="wrapper-additional-info">
@@ -36,18 +37,37 @@
                   <img src={cifrao} alt="#" />
               </div>
               <div class='situation'>
-                  <p>{type}</p>
+                  {#if data.Aluguel != null}
+                    <p>Aluguel</p>
+                    {:else if data.Venda != null}
+                    <p>Venda</p>
+                  {/if}
               </div>
           </div>
           <div class='buy-button'>
-            <div class="flex justify-end items-end">
-              <p class="text-2xl font-bold">{Number.isInteger(valor) ? `${valor},00` : valor}</p>
-              {#if buttonType == 'Aluguel'}
-                <p class="text-md font-bold">/por dia</p>
+            <div class="flex flex-col justify-end items-end">
+              {#if data.promocao != null && data.promocao.status != 'Inativo'}
+                <p class="text-md line-through font-bold">{formatedValue}</p>
+
+                <p class="text-2xl font-bold">
+                  {promotionalValue}
+                  {#if data.Aluguel != null}
+                    <span class="text-sm">/por dia</span>
+                  {/if}
+                </p>
+              {/if}
+
+              {#if data.promocao == null || data.promocao.status == 'Inativo'}
+                <p class="text-2xl font-bold">
+                  {formatedValue}
+                  {#if data.Aluguel != null}
+                    <span class="text-sm">/por dia</span>
+                  {/if}
+                </p>
               {/if}
             </div>
-            <a href="/venda?produto_id={id}">
-              <button class="botao">{buttonType == 'Venda' ? 'Comprar' : 'Alugar'}</button>
+            <a href="/venda?produto_id={data.id}">
+              <button class="botao">{data.Aluguel != null ? 'Alugar' : 'Comprar'}</button>
             </a>
           </div>
       </div>
