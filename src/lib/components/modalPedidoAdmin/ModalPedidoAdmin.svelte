1a9865b321acf3b9ac31f1ac6fd769dc35b7b637
Thiago <thiagodavidpd@gmail.com>
2023-03-20 11:59:17 -0300

    create modal to user insert tracking  info

--- src/lib/components/modalPedidoAdmin/ModalPedidoAdmin.svelte
+++ src/lib/components/modalPedidoAdmin/ModalPedidoAdmin.svelte
@@ -150,6 +150,21 @@
           </div>
         </div>
 
+        <input type="checkbox" 
+        id="my-modal-{`${pedido.data_pedido}${pedido.id}${pedido.endereco.rua}${pedido.endereco.numeroRua}enviar`}" 
+        class="modal-toggle" 
+        />
+
+        <div class="modal">
+          <div class="modal-box relative">
+            <label 
+            for="my-modal-{`${pedido.data_pedido}${pedido.id}${pedido.endereco.rua}${pedido.endereco.numeroRua}enviar`}" 
+            class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+            <h3 class="text-lg font-bold">Informações do envio</h3>
+            <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+          </div>
+        </div>
+
         <div>
           {#if pedido.vendas.length > 0  || pedido.alugueis.length > 0}
             <div class="overflow-x-auto w-full">
