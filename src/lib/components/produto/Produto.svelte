553bcce3fcdc6d39279adea870e53588ffcce9bc
Thiago <thiagodavidpd@gmail.com>
2023-03-20 11:58:02 -0300

    change style to show discount on product

--- src/lib/components/produto/Produto.svelte
+++ src/lib/components/produto/Produto.svelte
@@ -24,13 +24,13 @@
           <h1 class="text-3xl font-bold">{data.nome}</h1>
           <p>{data.descricao}</p>
         </div>
-        {#if data.promocao != null}
+        {#if data.promocao != null && data.promocao.status != 'Inativo'}
           <div >
             <span class="bg-[#7C3267] rounded p-2 text-white">Item em Promoçao</span>
           </div>
         {/if}
       </div>
-      
+
       <div class="wrapper-additional-info">
           <div class='container-situation'>
               <div class='icon-situation'>
@@ -44,11 +44,19 @@
                   {/if}
               </div>
           </div>
-          <div class='buy-button'>
-            <div class="flex flex-col justify-end items-end">
-              {#if data.promocao != null && data.promocao.status != 'Inativo'}
-                <p class="text-md line-through font-bold">{formatedValue}</p>
 
+          <div class='buy-button'>
+            <div class="flex flex-col justify-end items-center">
+              {#if data.promocao != null && data.promocao.status != 'Inativo'}
+                <div class="flex gap-1 mb-1 justify-center items-center">
+                  {#if data.promocao.tipo == 'porcentual'}
+                    <p class="p-1 bg-[#7C3267] text-white rounded">-{data.promocao.valor_desconto}%</p>
+                  {:else}
+                    <p class="p-1 bg-[#7C3267] text-white rounded">-R${data.promocao.valor_desconto}</p>
+                  {/if}
+                  <p class="text-md line-through font-bold">{formatedValue}</p>
+                </div>
+                
                 <p class="text-2xl font-bold">
                   {promotionalValue}
                   {#if data.Aluguel.length > 0}
