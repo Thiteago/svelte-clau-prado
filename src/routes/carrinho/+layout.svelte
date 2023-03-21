c7c018d6edad7290ccfb71acdd4db672e5c2f837
thiteago <ti+thiago@seguroviagem.srv.br>
2023-03-20 22:03:37 -0300

    create layout to cart pages

---
+++ src/routes/carrinho/+layout.svelte
@@ -0,0 +1,14 @@
+<script>
+	import Header from "$lib/components/header/Header.svelte";
+  import { currentStep } from "$lib/js/stores/cart.js";
+  import Steps from "$lib/components/steps/Steps.svelte";
+</script>
+
+
+<Header tamanho="grande" />
+<div class="bg-white">
+  <div class="flex justify-center">
+    <Steps currentStep={$currentStep} />
+  </div>
+  <slot />
+</div>
