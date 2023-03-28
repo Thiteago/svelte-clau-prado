0c2eff3d1260eb397fc2a16f73cb4237329d8605
thiteago <ti+thiago@seguroviagem.srv.br>
2023-03-27 21:34:01 -0300

    refactoring header conditional

--- src/lib/components/header/Header.svelte
+++ src/lib/components/header/Header.svelte
@@ -27,112 +27,65 @@ function  handleRemoveItem(id){
 
 </script>
 <header class="main-header">
-  {#if !$signed}
-    <div class="container-left-content">
-        <div class="wrapper-logo {tamanho == "grande" ? "" : "-pequeno"}">
-        <img src={logo} alt="logo clau prado"/>
-        </div>
-        <nav class="nav">
-          <a class="main-action" href={"/#"}>Inicio</a>
-          <a class="main-action" href={"/produtos"}>Produtos</a>
-          <a class="main-action" href={"/sobre"}>Sobre</a>
-        </nav>
-    </div>
-    
 
-    <div class="container-right-content">
-      <!-- svelte-ignore a11y-click-events-have-key-events -->
-      <div on:click={() => carrinhoAtivo == true ? carrinhoAtivo = false : carrinhoAtivo = true} class="cart-container">
-        {#if !$page.url.pathname.includes("/carrinho")}
-          <img src={carts} alt="">
-          <span>{$cart.length > 0 ? `${$cart.length}` : ''} {$cart.length > 1 ? ` Itens  no carrinho` : $cart.length == 0 ? 'Nenhum item no carrinho' : ` Item no carrinho`}</span>
-        {/if}
-      </div>
-      {#if carrinhoAtivo == true}
-        <div class="cart-preview-container">
-          <h2 class="title">Alguns produtos no seu carrinho</h2>
-          <div class="wrapper-products">
-            {#if $cart.length > 0}
-              {#each $cart as produto}
-                <div class="produto">
-                  <div class="produto-info">
-                    <h3>{produto.nome}</h3>
-                    <!-- svelte-ignore a11y-click-events-have-key-events -->
-                    <span on:click={handleRemoveItem(produto.id)} class="trash"><img width="25" height="25" src={trash} alt="icone de lata de lixo"></span>
-                  </div>
+  <div class="container-left-content">
+    <div class="wrapper-logo {tamanho == "grande" ? "" : "-pequeno"}">
+      <img src={logo} alt="logo clau prado"/>
+    </div>
+    <nav class="nav">
+      <a class="main-action" href={"/#"}>Inicio</a>
+      <a class="main-action" href={"/produtos"}>Produtos</a>
+      <a class="main-action" href={"/sobre"}>Sobre</a>
+    </nav>
+  </div>
+
+  <div class="container-right-content">
+    <div class="cart-container">
+      {#if $page.url.pathname != "/carrinho"}
+        <!-- svelte-ignore a11y-click-events-have-key-events -->
+        <img on:click={() => carrinhoAtivo == true ? carrinhoAtivo = false : carrinhoAtivo = true} src={carts} alt="">
+        <span>{$cart.length > 0 ? `${$cart.length}` : ''} {$cart.length > 1 ? ` Itens  no carrinho` : $cart.length == 0 ? 'Nenhum item no carrinho' : ` Item no carrinho`}</span>
+      {/if}
+    </div>
+    {#if carrinhoAtivo == true}
+      <div class="cart-preview-container">
+        <h2 class="title">Alguns produtos no seu carrinho</h2>
+        <div class="wrapper-products">
+          {#if $cart.length > 0}
+            {#each $cart as produto}
+              <div class="produto">
+                <div class="produto-info">
+                  <h3>{produto.nome}</h3>
+                  <!-- svelte-ignore a11y-click-events-have-key-events -->
+                  <span on:click={handleRemoveItem(produto.id)} class="trash"><img width="25" height="25" src={trash} alt="icone de lata de lixo"></span>
                 </div>
-              {/each}
-            {:else}
-              <div class="text-center mt-2">
-                <h3>Nenhum produto no carrinho</h3>
               </div>
-            {/if}
-            <div class="wrapper-button-cart">
-              <a href="/carrinho">Ir para carrinho</a>
+            {/each}
+          {:else}
+            <div class="text-center mt-2">
+              <h3>Nenhum produto no carrinho</h3>
             </div>
+          {/if}
+          <div class="wrapper-button-cart">
+            <a href="/carrinho">Ir para carrinho</a>
           </div>
         </div>
-      {/if}
-      
-      <div class="wrapper-login">
-        <a class="main-action -second" href={"/login"}>Login</a>
-        <a class="main-action -third" href={"/login"}>ou Cadastre-se</a>
-        <img src={avatar} alt="imagem para cadastro" height="105" class="img_avatar"/>
       </div>
+    {/if}
+    {#if $signed == false}
+    <div class="wrapper-login">
+      <a class="main-action -second" href={"/login"}>Login</a>
+      <a class="main-action -third" href={"/login"}>ou Cadastre-se</a>
+      <img src={avatar} alt="imagem para cadastro" height="105" class="img_avatar"/>
     </div>
-  {:else}
-    <div class="container-left-content">
-      <div class="wrapper-logo {tamanho == "grande" ? "" : "-pequeno"}">
-        <img src={logo} alt="logo clau prado"/>
-      </div>
-      <nav class="nav">
-        <a class="main-action" href={"/#"}>Inicio</a>
-        <a class="main-action" href={"/produtos"}>Produtos</a>
-        <a class="main-action" href={"/sobre"}>Sobre</a>
-      </nav>
-    </div>
-
-    <div class="container-right-content">
-      <div class="cart-container">
-        {#if $page.url.pathname != "/carrinho"}
-          <!-- svelte-ignore a11y-click-events-have-key-events -->
-          <img on:click={() => carrinhoAtivo == true ? carrinhoAtivo = false : carrinhoAtivo = true} src={carts} alt="">
-          <span>{$cart.length > 0 ? `${$cart.length}` : ''} {$cart.length > 1 ? ` Itens  no carrinho` : $cart.length == 0 ? 'Nenhum item no carrinho' : ` Item no carrinho`}</span>
-        {/if}
-      </div>
-      {#if carrinhoAtivo == true}
-        <div class="cart-preview-container">
-          <h2 class="title">Alguns produtos no seu carrinho</h2>
-          <div class="wrapper-products">
-            {#if $cart.length > 0}
-              {#each $cart as produto}
-                <div class="produto">
-                  <div class="produto-info">
-                    <h3>{produto.nome}</h3>
-                    <div class="adicional-info-wrapper">
-                      <span><img src={trash} alt="icone de lata de lixo"></span>
-                    </div>
-                  </div>
-                </div>
-              {/each}
-            {:else}
-              <div class="text-center mt-2">
-                <h3>Nenhum produto no carrinho</h3>
-              </div>
-            {/if}
-            <div class="wrapper-button-cart">
-              <a href="/carrinho">Ir para carrinho</a>
-            </div>
-          </div>
-        </div>
-      {/if}
+    {:else}
       <div class="wrapper-login -logado">
-        <a href={"/login"} class="main-action -secondlogado">Ola! <br /> {$user.nome.split(' ')[0]}</a>
+        <a href='/perfil' class="main-action -secondlogado">Ola! <br /> {$user.nome.split(' ')[0]}</a>
         <div class="container-options">
           <a class="sair-btn" href={"/perfil"}>Minha conta</a>
           <a class="sair-btn" href={"/"} on:click={logout}>Sair</a>
         </div>
       </div>
-    </div>
-  {/if}
+    {/if}
+  </div>
 </header>
