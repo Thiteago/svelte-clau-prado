be9a65f8f06309280e1a5c361bfba32da2bf6d81
thiteago <ti+thiago@seguroviagem.srv.br>
2023-03-27 21:39:26 -0300

    removing unecessary header import and removing cart localStorage on step4

--- src/routes/carrinho/step4/+page.svelte
+++ src/routes/carrinho/step4/+page.svelte
@@ -1,22 +1,15 @@
 <script>
-  import Header from "$lib/components/header/Header.svelte";
-  import Steps from "$lib/components/steps/Steps.svelte";
   import {cart, currentStep} from "$lib/js/stores/cart.js";
 
   $cart = []
   $currentStep = 4;
+  localStorage.removeItem('cart')
+  
 </script>
 
-<Header />
-
 <div class="w-full bg-white flex flex-col items-center">
-  <div class="flex justify-center">
-    <Steps currentStep={$currentStep} />
-  </div>
   <div class="mt-10">
     <h1 class="text-2xl font-bold text-center mt-6">Obrigado por comprar conosco!</h1>
     <p class="text-center mt-4">Seu pedido foi realizado com sucesso.</p>
-
   </div>
-
 </div>
