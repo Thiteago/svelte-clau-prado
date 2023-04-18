9cb3338d4c2a5eb5693c0df594e377987f5f999f
thiteago <ti+thiago@seguroviagem.srv.br>
2023-04-17 22:16:52 -0300

    W.I.P - Create report to show more specific data about the products and they sales

---
+++ src/routes/perfil/admin/relatorios/desempenho/+page.svelte
@@ -0,0 +1,38 @@
+<script>
+  import { PUBLIC_BACKEND_URL } from '$env/static/public'
+	import { onMount } from 'svelte';
+
+  let produtos = []
+
+  onMount(async () => {
+    const response = await fetch(`${PUBLIC_BACKEND_URL}/relatorio/desempenho`);
+    produtos = await response.json();
+    console.log(produtos)
+  });
+
+</script>
+
+<section class="flex justify-center w-full">
+  {#if produtos.length > 0}
+    <table class="table table-striped w-full mt-5">
+      <thead>
+        <tr>
+          <th scope="col">Produto</th>
+          <th scope="col">Quantidade vendida</th>
+          <th scope="col">Quantidade alugada</th>
+          <th scope="col">Quantidade visualizada</th>
+          <th scope="col">Valor total</th>
+        </tr>
+      </thead>
+      <tbody>
+        {#each produtos as produto}
+          <tr>
+            <td>{produto.nome}</td>
+            <td>{produto.quantidadeVendida}</td>
+            <td>{produto.valorTotal}</td>
+          </tr>
+        {/each}
+      </tbody>
+    </table>
+  {/if}
+</section>
