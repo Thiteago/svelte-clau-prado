6f08aac7f6d5a4249d78fddfe2d4f36ab03de43e
thiteago <ti+thiago@seguroviagem.srv.br>
2023-04-18 22:03:45 -0300

    finished product performance report

---
+++ src/lib/components/productPerformanceCard/ProductPerformanceCard.svelte
@@ -0,0 +1,61 @@
+<script>
+  import { Doughnut} from 'svelte-chartjs'
+  import { Chart as ChartJS, Title, Tooltip, Legend, LineElement, LinearScale, PointElement, CategoryScale, ArcElement } from 'chart.js';
+  ChartJS.register(Title, Tooltip, Legend, LineElement, LinearScale, PointElement, CategoryScale, ArcElement);
+
+  export let produto
+  let produtoInfo = []
+  produtoInfo.push(produto.statisticProduct.qtdeVendida)
+  produtoInfo.push(produto.statisticProduct.qtdeAlugada)
+  produtoInfo.push(produto.statisticProduct.qtdeVisualizada)
+  
+  console.log(produtoInfo)
+  let data = {
+    labels: ["Quantidade Vendida", "Quantidade Alugada", "Quantidade Visualizada"],
+    datasets: [
+      {
+        data: produtoInfo,
+        backgroundColor: ["#F7464A", "#46BFBD", "#FDB45C"],
+        hoverBackgroundColor: [
+          "#FF5A5E",
+          "#5AD3D1",
+          "#FFC870",
+        ]
+      }
+    ]
+  };
+
+  let options = {
+    responsive: true
+  }
+</script>
+
+<div class="flex gap-6 w-3/4 bg-gray-100 rounded-xl">
+  <div class="w-3/12">
+    <Doughnut {data} {options}/>
+  </div>
+  <div class="flex flex-col justify-between py-2 w-6/12">
+    <h1 class="text-xl font-bold">{produto.nome}</h1>
+    <div class="flex gap-3">
+      <div class="flex flex-col">
+        <p class="flex gap-1">💸 Quantidade Vendida </p>
+        <p class="flex gap-1">🏠 Quantidade Alugado </p>
+        <p class="flex gap-1">👀 Quantidade Visualizada </p>
+      </div>
+      <div class="flex items-center flex-col">
+        <p class="flex gap-1">{produto.statisticProduct.qtdeVendida}</p>
+        <p class="flex gap-1">{produto.statisticProduct.qtdeAlugada}</p>
+        <p class="flex gap-1">{produto.statisticProduct.qtdeVisualizada}</p>
+      </div>
+    </div>
+  </div>
+  <div class="flex flex-col gap-5 py-3 pr-3 w-3/12">
+    <div >
+      <img src="http://localhost:3333/static/{produto.imagens[0]}" alt="">
+    </div>
+    <div class="flex justify-center flex-col items-center">
+      <p class="font-bold">Lucro total:</p>
+      <p class="text-3xl">R$ {produto.statisticProduct.totalLucro}</p>
+    </div>
+  </div>
+</div>
--- src/routes/perfil/admin/relatorios/desempenho/+page.svelte
+++ src/routes/perfil/admin/relatorios/desempenho/+page.svelte
@@ -1,40 +1,25 @@
 <script>
   import { PUBLIC_BACKEND_URL } from '$env/static/public'
 	import { onMount } from 'svelte';
+	import ProductPerformanceCard from '$lib/components/productPerformanceCard/ProductPerformanceCard.svelte';
 
   let produtos = []
 
   onMount(async () => {
     const response = await fetch(`${PUBLIC_BACKEND_URL}/relatorio/desempenho`);
     produtos = await response.json();
-    console.log(produtos)
   });
 
 </script>
 
-<section class="flex justify-center w-full">
+<section class="flex justify-center w-full mt-4">
   {#if produtos.length > 0}
-    <table class="table table-striped w-full mt-5">
-      <thead>
-        <tr>
-          <th scope="col">Produto</th>
-          <th scope="col">Quantidade vendida</th>
-          <th scope="col">Quantidade alugada</th>
-          <th scope="col">Quantidade visualizada</th>
-          <th scope="col">Valor total</th>
-        </tr>
-      </thead>
-      <tbody>
-        {#each produtos as produto}
-          <tr>
-            <td>{produto.nome}</td>
-            <td>{produto.statisticProduct.qtdeVendida}</td>
-            <td>{produto.statisticProduct.qtdeAlugada}</td>
-            <td>{produto.statisticProduct.qtdeVisualizada}</td>
-            <td>{produto.statisticProduct.totalLucro}</td>
-          </tr>
-        {/each}
-      </tbody>
-    </table>
+    {#each produtos as produto }
+      <ProductPerformanceCard {produto} />
+    {/each}
+  {:else}
+    <div class="flex justify-center w-full">
+      <h1 class="text-2xl font-bold">Nenhum produto encontrado</h1>
+    </div>
   {/if}
 </section>
