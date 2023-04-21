945471e84d58f4dda9c80e94c68bfdda61e7e6e5
Thiago <thiagodavidpd@gmail.com>
2023-04-20 23:40:23 -0300

    create new footer

---
+++ src/lib/components/footer/Footer.svelte
@@ -0,0 +1,17 @@
+<script>
+  import smallLogo from '$lib/assets/img/logo-clau.png'
+</script>
+
+<footer class="w-full items-center flex flex-col bg-[#632c52]">
+  <div class="w-8/12 pt-6">
+      <a href="/" class="flex items-end font-bold text-white gap-4 text-3xl">
+        <img width=100 src={smallLogo} alt="logo">
+        ClauPrado
+      </a>
+  </div>
+  <div class="w-8/12 pt-12 flex flex-col gap-2">
+    <p class="text-white">📞 (12)997431974</p>
+    <p class="text-white">✉️ clauprado@gmail.com</p>
+    <p class="text-white">🚚 Entrega para todo o <strong>Vale do Paraíba</strong></p>
+  </div>
+</footer>
