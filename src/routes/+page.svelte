68118dc1e1095c5038e9967b44dceaefe5b6cf61
Thiago <thiagodavidpd@gmail.com>
2023-04-20 23:42:48 -0300

    W.I.P - Create new initial page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,30 +1,41 @@
 <script>
 import { PUBLIC_BACKEND_URL } from '$env/static/public'
-import Carousel from 'svelte-carousel'
+import quotes from '$lib/assets/icons/quotes.svg'
+import leftquote from '$lib/assets/icons/left-quote.svg'
 import Header from '$lib/components/header/Header.svelte'
 import SubHeader from '$lib/components/subHeader/SubHeader.svelte'
-import Slide from '../lib/components/slide/Slide.svelte'
-import BuyInfo from '../lib/components/buyinfo/Buyinfo.svelte'
+import Slide from '$lib/components/slide/Slide.svelte'
+import BuyInfo from '$lib/components/buyinfo/Buyinfo.svelte'
 import {loadStorageData} from '$lib/js/stores/login.js'
 import { registerVisit,fetchProducts } from '$lib/js/helpers.js'
 import './style.scss'
+import Carousel from 'svelte-carousel'
+import { browser } from '$app/environment';
 import { onMount } from 'svelte';
+	import Footer from '$lib/components/footer/Footer.svelte';
 
 let hasSpecialSale = false
 let products = []
+let promotionalProduts = []
 
 async function checkIfHasSpecialSale(){
     const response = await fetch(`${PUBLIC_BACKEND_URL}/promocao/promocoesativas`)
-		let data = response.json()
+		let data = await response.json()
+    console.log(data)
     if(data.length > 0){
 			hasSpecialSale = true
+      data.forEach(promo => {
+        promo.produtos.forEach(prod => {
+          promotionalProduts.push(prod)
+        })
+      })
+      promotionalProduts = promotionalProduts.slice(0,4)
 		}
 }
 
 async function orderProducts(){
   products = await fetchProducts()
   //sort products by date most recent and return 8 products
-  console.log(products)
   products.sort((a,b) => {
     return new Date(b.dataPublicacao) - new Date(a.dataPublicacao)
   })
@@ -34,10 +45,10 @@ async function orderProducts(){
 }
 
 onMount(async () => {
-    loadStorageData()
-    registerVisit()
-    checkIfHasSpecialSale()
-    orderProducts()
+    await loadStorageData()
+    await registerVisit()
+    await checkIfHasSpecialSale()
+    await orderProducts()
 })
 
 </script>
@@ -48,6 +59,42 @@ onMount(async () => {
  
   <div class="container-Content">
 		<BuyInfo />
+
+    {#if hasSpecialSale}
+      <section class="flex flex-col w-9/12 m-auto bg-[#faedf8] rounded-lg py-6 px-2">
+        <div class="pl-2">
+          <h1 class="font-medium  text-3xl">Produtos com precinho especial</h1>
+          <p>Os itens abaixo estão todos em promoção, confira! 🥳🎉</p>
+        </div>
+
+        <div class="mt-4 flex gap-3 flex-wrap pl-2">
+          {#if products.length > 0}
+            {#each promotionalProduts as prod }
+              <div>
+                <div class="flex justify-center bg-[#f5d9f1] hover:bg-[#e092cf] cursor-pointer py-12 rounded-md">
+                  <a href="/venda?produto_id={prod.id}">
+                    <img width="250" src="{PUBLIC_BACKEND_URL}/static/{prod.imagens[0]}" alt="">
+                  </a>
+                </div>
+                <div class="flex flex-col mt-2 items-center">
+                  <div class="font-bold"><a href="/venda?produto_id={prod.id} ">{prod.nome}</a></div>
+                  <div class="font-bold">R$ {prod.valor}</div>
+                </div>
+              </div>
+            {/each}
+          {:else}
+            <div class="flex justify-center items-center bg-[#F0F0F0]cursor-pointer py-12">
+              <div class="font-bold">Nenhum produto encontrado</div>
+            </div>
+          {/if}
+        </div>
+        <div class="flex justify-center">
+          <a href="/produtos" class="flex rounded-lg justify-center w-2/12 items-center bg-[#b14897] hover:bg-[#92397a] cursor-pointer mt-3 py-3">
+            <div class="font-bold text-white">Ver todos os produtos</div>
+          </a>
+        </div>
+      </section>
+    {/if}
     <section class="flex flex-col w-9/12 m-auto mt-6 gap-4 ">
       <div>
         <h1 class="font-italic text-3xl">Novos Produtos</h1>
@@ -59,10 +106,14 @@ onMount(async () => {
           {#each products as prod }
             <div>
               <div class="flex justify-center bg-[#F0F0F0] hover:bg-[#c6c6c6] cursor-pointer py-12">
-                <img width="250" src="http://localhost:3333/static/{prod.imagens[0]}" alt="">
+                <a href="/venda?produto_id={prod.id}">
+                  <img width="250" src="{PUBLIC_BACKEND_URL}/static/{prod.imagens[0]}" alt="">
+                </a>
               </div>
               <div class="flex flex-col mt-2 items-center">
-                <div class="font-bold">{prod.nome}</div>
+                <div class="font-bold">
+                  <a href="/venda?produto_id={prod.id}">{prod.nome}</a>
+                </div>
                 <div class="font-bold">R$ {prod.valor}</div>
               </div>
             </div>
@@ -73,15 +124,55 @@ onMount(async () => {
           </div>
         {/if}
       </div>
-      <div>
-        <a href="/produtos" class="flex rounded-lg justify-center items-center bg-[#F0F0F0] hover:bg-[#c6c6c6] cursor-pointer mt-3 py-3">
+      <div class="flex justify-center">
+        <a href="/produtos" class="flex rounded-lg justify-center w-2/12 items-center bg-[#F0F0F0] hover:bg-[#c6c6c6] cursor-pointer mt-3 py-3">
           <div class="font-bold">Ver todos os produtos</div>
         </a>
       </div>
     </section>
 
-    <section>
-      
+    <section class="w-9/12 m-auto mt-6">
+      <div>
+        <h1 class="font-italic text-3xl">Depoimentos</h1>
+        <p>Leia depoimentos de outros clientes que tiveram a experiência completa com nossos produtos! 🥹</p>
+      </div>
+
+      <div style="min-height: 12rem" class="flex items-center">
+        {#if browser}
+        <Carousel
+        particlesToShow={3}
+        particlesToScroll={2}
+        autoplayDuration={5000}
+        duration={5000}
+        autoplay
+        pauseOnFocus={true}
+        dots={false}
+        arrows={false}
+
+        >
+          <div class="flex flex-col items-center">
+            <h1 class="font-bold text-2xl flex">
+              <i class="w-3">
+                <img class="w-6" src={leftquote} alt=""/>
+              </i>
+                Sensacional 
+                <i class="w-3"><img class="w-6" src={quotes} alt=""/>
+              </i>
+            </h1>
+            <p>aidqkadiqeiwqeiqweiqwieqwi</p>
+          </div>
+          <div class="flex flex-col items-center">
+            <h1 class="font-bold text-2xl">Sensacional</h1>
+            <p>aidqkadiqeiwqeiqweiqwieqwi</p>
+          </div>
+          <div class="flex flex-col items-center">
+            <h1 class="font-bold text-2xl">Sensacional</h1>
+            <p>aidqkadiqeiwqeiqweiqwieqwi</p>
+          </div>
+        </Carousel>
+        {/if}
+      </div>
     </section>
   </div>
 </div>
+<Footer/>
