2d54bcb0f86324f4516372dacc351a71f4ab17e8
Thiago <thiagodavidpd@gmail.com>
2023-04-21 01:07:35 -0300

    finish new footer

--- src/lib/components/footer/Footer.svelte
+++ src/lib/components/footer/Footer.svelte
@@ -1,5 +1,6 @@
 <script>
   import smallLogo from '$lib/assets/img/logo-clau.png'
+  import paypal from '$lib/assets/icons/paypal.svg'
 </script>
 
 <footer class="w-full items-center flex flex-col bg-[#632c52]">
@@ -9,9 +10,19 @@
         ClauPrado
       </a>
   </div>
-  <div class="w-8/12 pt-12 flex flex-col gap-2">
-    <p class="text-white">📞 (12)997431974</p>
-    <p class="text-white">✉️ clauprado@gmail.com</p>
-    <p class="text-white">🚚 Entrega para todo o <strong>Vale do Paraíba</strong></p>
+  <div class="w-8/12 py-12 flex justify-between ">
+    <div class="flex flex-col gap-2">
+      <p class="text-white">📞 (12)997431974</p>
+      <p class="text-white">✉️ clauprado@gmail.com</p>
+      <p class="text-white">🚚 Entrega para todo o <strong>Vale do Paraíba</strong></p>
+    </div>
+    <div class="flex flex-col items-end">
+      <span>
+        <p class="text-white">Compre com segurança</p>
+        <p class="text-white text-right">Pagamento via Paypal</p>
+      </span>
+      <img width=50 src={paypal} alt="paypal">
+    </div>
+
   </div>
 </footer>
