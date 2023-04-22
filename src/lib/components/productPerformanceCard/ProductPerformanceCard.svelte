aeb7af743f90fc65110298748950f8b6589d6cf4
Thiago <thiagodavidpd@gmail.com>
2023-04-22 17:37:54 -0300

    adjust style of the product performance component

--- src/lib/components/productPerformanceCard/ProductPerformanceCard.svelte
+++ src/lib/components/productPerformanceCard/ProductPerformanceCard.svelte
@@ -9,7 +9,6 @@
   produtoInfo.push(produto.statisticProduct.qtdeAlugada)
   produtoInfo.push(produto.statisticProduct.qtdeVisualizada)
   
-  console.log(produtoInfo)
   let data = {
     labels: ["Quantidade Vendida", "Quantidade Alugada", "Quantidade Visualizada"],
     datasets: [
@@ -25,14 +24,19 @@
     ]
   };
 
+
   let options = {
     responsive: true
   }
 </script>
 
-<div class="flex gap-6 w-3/4 bg-gray-100 rounded-xl">
-  <div class="w-3/12">
-    <Doughnut {data} {options}/>
+<div class="flex gap-6 w-2/4 bg-gray-100 rounded-xl">
+  <div class="w-3/12 flex items-center pl-2">
+    {#if produto.statisticProduct.qtdeVendida > 0 || produto.statisticProduct.qtdeAlugada > 0 || produto.statisticProduct.qtdeVisualizada > 0}
+      <Doughnut {data} {options}/>
+    {:else}
+      <p class="text-center">Nenhum evento para este item</p>
+    {/if}
   </div>
   <div class="flex flex-col justify-between py-2 w-6/12">
     <h1 class="text-xl font-bold">{produto.nome}</h1>
@@ -49,10 +53,13 @@
       </div>
     </div>
   </div>
-  <div class="flex flex-col gap-5 py-3 pr-3 w-3/12">
-    <div >
-      <img src="http://localhost:3333/static/{produto.imagens[0]}" alt="">
-    </div>
+  <div class="flex flex-col justify-center gap-5 py-3 pr-3 w-3/12">
+    {#if produto.imagens.length > 0}
+      <div class="flex justify-center">
+        <img width="70" src="http://localhost:3333/static/{produto.imagens[0]}" alt="">
+      </div>
+    {/if}
+
     <div class="flex justify-center flex-col items-center">
       <p class="font-bold">Lucro total:</p>
       <p class="text-3xl">R$ {produto.statisticProduct.totalLucro}</p>
