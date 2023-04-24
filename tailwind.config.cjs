857d0c35b44c842b43c4263dca288d0339b70e5e
thiteago <ti+thiago@seguroviagem.srv.br>
2023-04-24 17:38:34 -0300

    added new value of gap and new screens sizes to tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,16 +2,26 @@
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl' : '1280px',
+      '2xl' : '1536px',
+      '3xl' : '1920px',
+      '4xl' : '2560px',
+    },
     fontFamily: {
       sans: ['poppins', 'sans-serif'],
     },
     extend: {
-      zIndex: {
-        '10': '10',
-      },
       inset : {
         '12%' : '12%',
-      }
+      },
+      gap: {
+        '0.5' : '0.125rem',
+      },
+
     },
   },
   plugins: [require("daisyui")],
