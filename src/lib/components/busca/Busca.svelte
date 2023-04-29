7dcc063f73529e9458de209f595b5df27a405cdb
Thiago <thiagodavidpd@gmail.com>
2023-04-29 15:39:45 -0300

    remove unused button on search

--- src/lib/components/busca/Busca.svelte
+++ src/lib/components/busca/Busca.svelte
@@ -6,6 +6,5 @@
 <section class="w-full flex ">
   <div class="w-full justify-end flex">
       <input class="w-3/4 rounded-lg p-2 sm:w-2/4" bind:value={busca} type="text" placeholder="Digite aqui o que deseja buscar" />
-      <button class="w-1/4 bg-[#7C3267] text-white rounded">Buscar</button>
   </div>
 </section>
