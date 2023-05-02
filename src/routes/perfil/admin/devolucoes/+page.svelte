d7ed33dbc9c3e2bc056f51abfc2750ca540fa226
Thiago <thiagodavidpd@gmail.com>
2023-05-02 17:10:26 -0300

    W.I.P - Create devoluções page

---
+++ src/routes/perfil/admin/devolucoes/+page.svelte
@@ -0,0 +1,14 @@
+<div>
+  <h1 class="text-2xl font-bold">Devoluções</h1>
+
+  <div>
+    <div>
+      pedidos a serem devolvidos nos proximos 7 dias
+    </div>
+    <div>
+      pedidos atrasados para devolução
+    </div>
+  </div>
+
+  
+</div>
