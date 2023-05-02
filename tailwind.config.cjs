8546926a56a21b7da8a036b5e83b2953b91fcb9d
Thiago <thiagodavidpd@gmail.com>
2023-05-02 16:37:02 -0300

    create new breakpoint on tailwind screens

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,9 +7,10 @@ module.exports = {
       'md': '768px',
       'lg': '1024px',
       'xl' : '1280px',
-      '2xl' : '1536px',
-      '3xl' : '1920px',
-      '4xl' : '2560px',
+      '2xl' : '1440px', 
+      '3xl' : '1536px',
+      '4xl' : '1920px',
+      '5xl' : '2560px',
     },
     fontFamily: {
       sans: ['poppins', 'sans-serif'],
