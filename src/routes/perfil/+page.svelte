1d9eac760502530a14fb5496e55f640ec1522750
thiteago <ti+thiago@seguroviagem.srv.br>
2023-05-02 21:22:17 -0300

    now show last rented orders and if user have some delivery of rented items pended

--- src/routes/perfil/+page.svelte
+++ src/routes/perfil/+page.svelte
@@ -1,14 +1,55 @@
 <script>
   import './perfil.scss'
   import { user } from '$lib/js/stores/login.js'
-  import { fetchOrdersByUserId } from '$lib/js/helpers.js'
+  import { fetchOrdersByUserId, formatDate, updateDevolution } from '$lib/js/helpers.js'
 	import { onMount } from 'svelte';
 
+  $: flashMessage = ''
+  let selectedPedidoId = ''
+  let selectedAluguelId = ''
+  
   let orders = []
+  $: rentedOrders = []
+  $: codigorastreio = ''
+  let lateReturn = []
+
+  function verifyExpiration(){
+    rentedOrders.forEach(order => {
+      order.alugueis.map(aluguel => {
+        let dataExpiracao = new Date(aluguel.data_expiracao).getTime()
+        if(dataExpiracao < new Date()){
+          lateReturn = [...lateReturn, aluguel]
+        }
+      })
+    })
+
+  }
+
+  async function handleSended(id, idAluguel){
+    if (await updateDevolution(id, idAluguel ,codigorastreio)){
+      flashMessage = 'Pedido atualizado com sucesso'
+      setTimeout(() => {
+        flashMessage = ''
+      }, 3000);
+    }else{
+      flashMessage = 'Erro ao atualizar pedido'
+      setTimeout(() => {
+        flashMessage = ''
+      }, 3000);
+    }
+  }
+  
 
 
   onMount(async () => {
     orders = await fetchOrdersByUserId($user.id) 
+    orders.forEach(order => {
+      if(order.alugueis.length > 0){
+        rentedOrders = [...rentedOrders, order]
+      }
+    })
+    verifyExpiration()
+    orders = orders.slice(0, 3)
   })
   
   
@@ -22,11 +63,71 @@
         <span>{$user.email}</span>
     </div>
   </div>
+
+  {#if rentedOrders.length >= 1}
+    <div class="flex justify-center">
+      <h2 class="text-2xl font-bold">Aluguéis Ativos ⬇️</h2>
+    </div>
+    <div class="grid-alugueis-container justify-center">
+      <div class="header col-3">Número do Pedido</div>
+      <div class="header col-3">Data de Aluguel</div>
+      <div class="header col-3">Data de Expiração</div>
+      {#each rentedOrders as order}
+        {#each order.alugueis as aluguel}
+          <div class="item col-3">{order.id}</div>
+          <div class="item col-3">{formatDate(aluguel.data_aluguel)}</div>
+          <div class="item col-3">{formatDate(aluguel.data_expiracao)}</div>
+        {/each}
+      {/each}
+    </div>
+
+
+    <div class="flex justify-center">
+      <h2 class="text-2xl font-bold">Devoluçõess atrasados 😢</h2>
+    </div>
+
+    {#if lateReturn.length > 0}
+      <div class="grid-container justify-center">
+        <div class="header col-4">Número do Pedido</div>
+        <div class="header col-4">Data do Aluguel</div>
+        <div class="header col-4">Dias atrasados</div>
+        <div class="header col-4">Informar Devolução</div>
+        {#each lateReturn as order}
+          <div class="item col-4">{order.pedidoId}</div>
+          <div class="item col-4">{formatDate(order.data_aluguel)}</div>
+          <div class="item col-4">{Math.floor((new Date() - new Date(order.data_expiracao)) / (1000 * 60 * 60 * 24))}</div>
+          <!-- svelte-ignore a11y-click-events-have-key-events -->
+          <label for="my-modal-3" on:click={() => {selectedPedidoId = order.pedidoId, selectedAluguelId = order.id}} style="cursor: pointer;" class="item col-4">Informar</label>
+        {/each}
+      </div>
+      <input type="checkbox" id="my-modal-3" class="modal-toggle" />
+      <label for="my-modal-3" class="modal cursor-pointer">
+        <label class="modal-box relative" for="">
+          {#if flashMessage != ''}
+            <span>{flashMessage}</span>
+          {/if}
+          <h3 class="text-lg font-bold">Informar Envio</h3>
+          <div class="flex gap-3 items-center">
+            <label for="codigo-rastreio">Código de Rastreio:</label>
+            <input bind:value={codigorastreio} type="text" name="codigo-rastreio" class="input input-bordered">
+          </div>
+          <div class="mt-3">
+            <button on:click={handleSended(selectedPedidoId, selectedAluguelId)} class="btn btn-success">Confirmar</button>
+            <label for="my-modal-3" class="btn btn-error">Cancelar</label>
+          </div>
+        </label>
+      </label>
+    {:else}
+      <div class="flex justify-center my-12">
+        Nenhuma devolução atrasada
+      </div>
+    {/if}
+  {/if}
   
   <div class="containerUltimoPedido">
     <div class="containerPedido">
       {#if orders.length > 0}
-        <h3 class="font-bold text-xl">últimos Pedidos</h3>
+        <h3 class="font-bold text-xl">Últimos Pedidos</h3>
         <div class="grid-container justify-center">
           <div class="header col-4">Número do Pedido</div>
           <div class="header col-4">Status</div>
