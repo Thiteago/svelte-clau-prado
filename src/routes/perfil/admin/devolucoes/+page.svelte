42cbe564a2e02a5c7c61582c4d18671e0652335e
thiteago <ti+thiago@seguroviagem.srv.br>
2023-05-02 21:21:34 -0300

    W.I.P - Continue creating of the devolucao page

--- src/routes/perfil/admin/devolucoes/+page.svelte
+++ src/routes/perfil/admin/devolucoes/+page.svelte
@@ -1,14 +1,62 @@
-<div>
+<script>
+  $: selectedType = 'Pendente'
+
+</script>
+
+
+<section class="p-6">
   <h1 class="text-2xl font-bold">Devoluções</h1>
 
   <div>
-    <div>
-      pedidos a serem devolvidos nos proximos 7 dias
-    </div>
-    <div>
-      pedidos atrasados para devolução
+    <label for="pedidos" class="label">
+      <span class="label-text">Selecione o tipo de pedidos a serem mostrados</span>
+    </label>
+    <select bind:value={selectedType} name="pedidos" class="select select-bordered">
+      <option value="Atrasado">Pendentes de Envio</option>
+      <option value="Devolvido">Enviados</option>
+      <option value="Todos">Todos</option>
+    </select>
+    <div class="flex flex-col items-center">
+      <table class="table cazuza table-zebra w-full mt-5">
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Cliente</th>
+            <th>Valor</th>
+            <th>Produto</th>
+            <th>Dias atrasados</th>
+            <th>Status</th>
+            <th>Cod. Rastreio</th>
+          </tr>
+        </thead>
+        <tbody>
+          {#if pedidos.length === 0}
+            <tr>
+              <td colspan="7">Nenhum pedido atrasado</td>
+            </tr>
+          {:else}
+            {#each displayedPedidos as pedido}
+              {#if pedido.status === selectedType || selectedType === 'Todos'}
+                <tr>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+              {/if}
+            {/each}
+          {/if}
+        </tbody>
+      </table>
+      <div class="btn-group mt-2">
+        {#if pages.length > 1}
+          {#each pages as page}
+            <button class="btn {currentPage == page ? 'btn-active' : ''}" on:click={() => {displayedPedidos = displayPedidos(page, selectedType)}}>{page}</button>
+          {/each}
+        {/if}
+      </div>
     </div>
   </div>
-
-  
-</div>
+</section>
