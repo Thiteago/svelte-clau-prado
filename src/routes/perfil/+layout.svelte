f82a7875d4cf19f3c71d831ffdc17566486042c1
thiteago <ti+thiago@seguroviagem.srv.br>
2023-05-16 21:25:40 -0300

    added the apropriate links to the profile hamburguer menu

--- src/routes/perfil/+layout.svelte
+++ src/routes/perfil/+layout.svelte
@@ -48,9 +48,21 @@
     {#if open}
       <div transition:fade class="menu-container">
         <ul style="padding-bottom: 1rem;" class="menu bg-base-100 w-full">
-          <li><a href="/">Inicio</a></li>
-          <li><a href="/produtos">Produtos</a></li>
-          <li><a href="/sobre">Sobre</a></li>
+          <li class="item" on:keyup={() => {goto('/')}} on:click={() => {goto('/')}}>Voltar</li>
+          <li class="item" on:keyup={() => {goto('/perfil')}} on:click={() => {goto('/perfil')}}>Perfil</li>
+          <li class="item" on:keyup={() => {goto('/perfil/dados')}} on:click={() => {goto('/perfil/dados')}}>Dados Cadastrais</li>
+          <li class="item" on:keyup={() => {goto('/perfil/enderecos')}} on:click={() => {goto('/perfil/enderecos')}}>Endereços</li>
+          <li class="item" on:keyup={() => {goto('/perfil/pedidos')}} on:click={() => {goto('/perfil/pedidos')}}>Historico de Pedidos</li>
+          {#if $user.cargo == 'Admin'}
+              <span class="py-10 text-center font-bold">Funções<br/> Administrativas <br/>↓</span>
+              <li class="item" on:keyup={() => {goto('/perfil/admin/pedidos')}} on:click={() => {goto('/perfil/admin/pedidos')}}>Pedidos</li>
+              <li class="item" on:keyup={() => {goto('/perfil/admin/devolucoes')}} on:click={() => {goto('/perfil/admin/devolucoes')}}>Devoluções</li>
+              <li class="item" on:keyup={() => {goto('/perfil/admin/promocoes')}} on:click={() => {goto('/perfil/admin/promocoes')}}>Promoções</li>
+              <li class="item" on:keyup={() => {goto('/perfil/admin/produtos')}} on:click={() => {goto('/perfil/admin/produtos')}}>Produtos</li>
+              <li class="item" on:keyup={() => {goto('/perfil/admin/relatorios')}} on:click={() => {goto('/perfil/admin/relatorios')}}>Relatórios</li>
+              <li class="item" on:keyup={() => {goto('/perfil/admin/despesas')}} on:click={() => {goto('/perfil/admin/despesas')}}>Fluxo de Despesas</li>
+              <li class="item" on:keyup={() => {goto('/perfil/admin/usuarios')}} on:click={() => {goto('/perfil/admin/usuarios')}}>Usuarios Administrativos</li>
+            {/if}
         </ul>
         <div class="w-full bg-white rounded-b-lg p-2">
           <div class="flex items-center justify-evenly w-full">
