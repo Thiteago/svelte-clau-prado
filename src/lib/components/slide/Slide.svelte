c2e5caab01c8bf3a03b275cf5799427c3b04b74f
thiteago <ti+thiago@seguroviagem.srv.br>
2023-05-30 20:30:09 -0300

    change Slide component to make him receive dynamic data instead of static

--- src/lib/components/slide/Slide.svelte
+++ src/lib/components/slide/Slide.svelte
@@ -1,14 +1,16 @@
 <script>
-  import {SlideData} from './SlideData.js'
+  import { PUBLIC_BACKEND_URL } from '$env/static/public'
   import edit from '$lib/assets/icons/edit.svg'
   import './style.scss'
   import nuvem from '$lib/assets/img/nuvem-fundo.png'
   import ModalEditSlide from '$lib/components/modalEditSlide/ModalEditSlide.svelte';
   import {user} from "$lib/js/stores/login.js"
+	import { onMount } from 'svelte';
 
 $: current = 0
+$: SlideData = []
+$: length = SlideData.length
 
-const length = SlideData.length
 
 function nextSlide() {
   if(current == length-1){
@@ -17,14 +19,24 @@ function nextSlide() {
     current += 1
   }
 }
-setInterval(nextSlide, 3000)
+
+async function fetchSlides(){
+  const response = await fetch(`${PUBLIC_BACKEND_URL}/slide/listar`)
+  const data = await response.json()
+  return data
+}
+
+onMount(async () => {
+  SlideData = await fetchSlides()
+  setInterval(nextSlide, 3000)
+})
 
 
 </script>
 
 
 
-<section class="main-slide relative">
+<section class="main-slide relative justify-center flex">
 
   {#if $user.cargo == 'Admin'}
     <label style="z-index: 300 " for="modal-edit" class="absolute cursor-pointer right-0 p-2 text-white gap-2 bg-black flex items-center">
@@ -32,39 +44,47 @@ setInterval(nextSlide, 3000)
       Editar Slide
     </label>
 
-    <ModalEditSlide />
+    <ModalEditSlide  on:changedSlide={async () => {SlideData = await fetchSlides()}}/>
   {/if}
-  <div class="wrapper-slide">
+
+  {#if SlideData.length > 0}
+    <div class="wrapper-slide">
       <div class="legenda">
         {#each SlideData as slide, i}
           <div class={i === current ? 'slide active' : 'slide'}>
             {#if i === current}
-              <h1> {slide.title} </h1>
+              <h1> {slide.titulo} </h1>
             {/if}
           </div>
         {/each}
         {#each SlideData as slide, i}
           <div class={i === current ? 'slide active' : 'slide'}>
             {#if i === current}
-            <p> {slide.describe} </p>
+            <p> {slide.descricao} </p>
           {/if}
           </div>
         {/each}
-          <div class="clouds">
-              <img class="max-w-none" src={nuvem} alt="nuvem esquerda"/>
-              <img class="max-w-none" src={nuvem} alt="nuvem direita"/>
-          </div>
+        <div class="clouds">
+          <img class="max-w-none" src={nuvem} alt="nuvem esquerda"/>
+          <img class="max-w-none" src={nuvem} alt="nuvem direita"/>
+        </div>
       </div>
       <img class="wrapper-slide -fundo" src={nuvem} alt=""/>
       {#each SlideData as slide, i}
         <div class={i === current ? 'slide active' : 'slide'}>
           {#if i === current}
-            <img class="wrapper-slide -mascara " id="teste" src={slide.image} alt="produto imagem"/>
+            <img class="wrapper-slide -mascara " id="teste" src={`${PUBLIC_BACKEND_URL}/static/slide/${slide.imagem}`} alt="produto imagem"/>
           {/if}
         </div>
       {/each}
-
-  </div>
+    </div>
+  
+  {:else}
+    <div class="absolute top-1/2 m-auto">
+      <h1> Não há slides cadastrados 😢 </h1>
+    </div>
+  {/if}
+  
 
   <div class="nuvens">
       <div class="Cloud"></div>
