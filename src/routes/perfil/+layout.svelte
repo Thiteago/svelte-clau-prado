e72cf6c28c0204a8eee9026b010145be1739d09f
thiteago <ti+thiago@seguroviagem.srv.br>
2023-06-05 20:51:01 -0300

    add categorias button

--- src/routes/perfil/+layout.svelte
+++ src/routes/perfil/+layout.svelte
@@ -34,6 +34,7 @@
         <li class="item" on:keyup={() => {goto('/perfil/pedidos')}} on:click={() => {goto('/perfil/pedidos')}}>Historico de Pedidos</li>
         {#if $user.cargo == 'Admin'}
           <span class="py-10 text-center font-bold">Funções<br/> Administrativas <br/>↓</span>
+          <li class="item" on:keyup={() => {goto('/perfil/admin/categorias')}} on:click={() => {goto('/perfil/admin/categorias')}}>Categorias</li>
           <li class="item" on:keyup={() => {goto('/perfil/admin/pedidos')}} on:click={() => {goto('/perfil/admin/pedidos')}}>Pedidos</li>
           <li class="item" on:keyup={() => {goto('/perfil/admin/devolucoes')}} on:click={() => {goto('/perfil/admin/devolucoes')}}>Devoluções</li>
           <li class="item" on:keyup={() => {goto('/perfil/admin/promocoes')}} on:click={() => {goto('/perfil/admin/promocoes')}}>Promoções</li>
---
+++ src/routes/perfil/admin/categorias/+page.svelte
@@ -0,0 +1,47 @@
+<script>
+
+  $: isUpdating = false
+  $: isCreating = false
+
+</script>
+
+<h2 class="text-3xl mb-3">Categorias</h2>
+<section class="w-full flex justify-between">
+  <div class="w-9/12">
+    <div>
+      <!-- svelte-ignore a11y-invalid-attribute -->
+      <a href="#" on:click={() => {isCreating = true}}>Cadastrar</a>
+    </div>
+    <div class="w-full mt-3">
+      <div class="w-full p-3 border-2">
+
+      </div>
+    </div>
+  </div>
+  <aside class="w-3/12 flex flex-col items-center px-2 ">
+    {#if isUpdating || isCreating}
+      {#if isUpdating}
+        <div class="w-full flex justify-between items-center">
+          <h3 class="font-bold">Alterar Categoria</h3>
+          <button on:click={() => {isUpdating = false}}>Cancelar</button>
+        </div>
+        <div class="w-full mt-3">
+          <div class="w-full p-3 border-2">
+
+          </div>
+        </div>
+      {:else}
+        <div class="w-full flex justify-between items-center">
+          <h3 class="font-bold">Cadastrar Categoria</h3>
+          <button on:click={() => {isCreating = false}}>Cancelar</button>
+        </div>
+        <div class="w-full mt-3 text-center rounded-lg">
+          <form>
+            <label for="nome">Nome da Categoria</label>
+            <input type="text" id="nome" name="nome" class="input">
+          </form>
+        </div>
+      {/if}
+    {/if}
+  </aside>
+</section>
