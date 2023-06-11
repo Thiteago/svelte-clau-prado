a2294e2a8b3a1cbfb20b6468f858e0021e3efe8e
Thiago <thiagodavidpd@gmail.com>
2023-06-11 17:51:31 -0300

    created input component

---
+++ src/lib/components/input_mudanca/Input_mudanca.svelte
@@ -0,0 +1,9 @@
+<script>
+  export let nome
+</script>
+
+<label for="{nome}">Insira {nome}</label>
+<input name="peso" type='text' class="input w-full border border-base-300" 
+bind:value={nome}
+required
+/>
