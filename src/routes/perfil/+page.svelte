59e70a5ef85281ecf2a7b942a2831f92b3ac9742
Thiago <thiagodavidpd@gmail.com>
2023-06-12 00:58:20 -0300

    fix dates ordenation

--- src/routes/perfil/+page.svelte
+++ src/routes/perfil/+page.svelte
@@ -78,6 +78,21 @@
       }, 3000);
     }
   }
+
+  function compareDates(date1, date2) {
+    const [day1, month1, year1] = date1.split('/').map(Number);
+    const [day2, month2, year2] = date2.split('/').map(Number);
+
+    if (year1 !== year2) {
+      return year1 - year2;
+    }
+
+    if (month1 !== month2) {
+      return month1 - month2;
+    }
+
+    return day1 - day2;
+  }
   
   async function loadChartData(content){
     maiorDiferenca = 0
@@ -109,6 +124,11 @@
     })
 
     labels = [...uniqueDates].sort((a, b) => new Date(a) - new Date(b));
+   
+    labels = labels.sort(compareDates)
+    
+
+
 
     labels.forEach((label) => {
       //store date with max profit
