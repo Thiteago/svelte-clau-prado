c4a8aedb97d2713e2135f019a7d1eac7d930a298
Thiago <thiagodavidpd@gmail.com>
2023-06-12 11:57:59 -0300

    pass color by params on previousbutton component

--- src/lib/components/previousbutton/Previousbutton.svelte
+++ src/lib/components/previousbutton/Previousbutton.svelte
@@ -1,11 +1,12 @@
 <script>
   import './previousButton.scss'
   export let endereco = ''
+  export let cor = ''
 </script>
 
 <a href={endereco}>
   <div class="container-botao">
     <a href="/#" class="voltar botao">&#8249;</a>
-    <p>Voltar</p>
+    <p class="{cor}">Voltar</p>
   </div>
 </a>
